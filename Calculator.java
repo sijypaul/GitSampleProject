55e935313791ca363eac106e6f03014c76610c28
sijypaul <104199522+sijypaul@users.noreply.github.com>
2023-07-18 15:11:57 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,12 +1,12 @@
 import java.util.*;
 public class Calculator
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
 		int a=20;
 		int b=2;
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the operators: (+,-,*,/)");
+		System.out.println("Enter the operators (+,-,*,/)::");
 		String optr=sc.nextLine();
 		if(optr.equals("+"))
 		{
