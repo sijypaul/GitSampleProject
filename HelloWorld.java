e7ebfeb7f7e3c09bcecca0df80a73926b778ea42
sijypaul <104199522+sijypaul@users.noreply.github.com>
2023-07-18 15:03:54 +0530

    added helloworld

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class HelloWorld
+{
+	public static void main(String args[]) throws IOException
+	{
+		
+		//DataInputStream d= new DataInputStream(System.in);
+		//System.out.println("Enter a number:");
+		int x=5;
+		if(x==5)
+		   System.out.println("Hello World....");
+		else
+		   System.out.println("Wrong Input...Enter again....");
+		
+	}
+}
